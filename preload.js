cd8d75c1c4bd7c92ab77630f8409e7ee589d9b98
Falkson <andreasfalk571@hotmail.com>
2026-03-01 18:52:59 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,9 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('notes', {
-  list:      ()         => ipcRenderer.invoke('notes:list'),
-  save:      (data)     => ipcRenderer.invoke('notes:save', data),
-  delete:    (filename) => ipcRenderer.invoke('notes:delete', filename),
-  get:       (filename) => ipcRenderer.invoke('notes:get', filename),
-  exportPDF: (data)     => ipcRenderer.invoke('notes:exportPDF', data)
+  list:        ()         => ipcRenderer.invoke('notes:list'),
+  save:        (data)     => ipcRenderer.invoke('notes:save', data),
+  delete:      (filename) => ipcRenderer.invoke('notes:delete', filename),
+  get:         (filename) => ipcRenderer.invoke('notes:get', filename),
+  exportPDF:   (data)     => ipcRenderer.invoke('notes:exportPDF', data),
+  getLogoPath: ()         => ipcRenderer.invoke('notes:getLogoPath')
 });
